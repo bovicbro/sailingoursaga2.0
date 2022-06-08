70c269236e6b2986b4d50310a350fa86fdeb2604
Victor Broman <victor@X230>
2022-06-08 19:28:23 +0200

    Adds mediakit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -15,7 +15,7 @@
     <Message
         title="Hi! So happy to see that you found your way to our adventure!"
         text="We are Victor and Antonia, a Swedish couple that always have dreamt about sailing towards the horizon. In August of 2021 we finally set off from our home port Hovenäset on this adventure of a lifetime."/>
-    <Image url="/saga-moored.jpg" text="Saga moored in home port Hovenäset" alt="Saga (boat) at a dock by red houses"/>
+    <Image url="signonsaga.jpg" text="Putting the name sign on Saga in Denmark" alt="Antonia and Victor putting on the name sign on Saga"/>
     <p>
         On @sailingoursaga on instagram we are documenting our journey, and would love it if you would want to follow and interact with us there for day-to-day updates.
     </p>
